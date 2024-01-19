a5927fc8da05c53d7a6a3aefea084f78134f7f14
Zwelius <fretzen.ragas@gmail.com>
2024-01-19 16:37:24 +0800

    functions on config

--- Phystats/config.php
+++ Phystats/config.php
@@ -9,10 +9,22 @@ if (!$connection) {
     die("Connection failed: " . mysqli_connect_error());
 }
 
-function nutristatus($bmi,$sex,$age){
-    if ($sex == "Male") {
-        
-    }else if ($sex == "Female"){
-        
+function zippertest_interpretation($zipper, $interpretation){
+    if ($zipper < 0) {
+        $interpretation = "Poor";
+    }else if ($zipper = 0){
+        $interpretation = "Needs Improvement";
+    }else if (0 < $zipper && $zipper < 2){
+        $interpretation = "Fair";
+    }else if (1.9 < $zipper && $zipper < 4){
+        $interpretation = "Good";
+    }else if (3.9 < $zipper && $zipper < 6){
+        $interpretation = "Very Good";
+    }else if (5.9 < $zipper){
+        $interpretation = "Excellent";
     }
+    return $interpretation;
+}
+function flexibility(){
+
 }
