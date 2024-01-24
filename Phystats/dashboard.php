9d7006bc9b25ee1a6bc2d5537febc61df6e178da
eeshanfandalala <ltfelfarra11@gmail.com>
2024-01-24 22:47:37 +0800

    nav added hihu

--- Phystats/dashboard.php
+++ Phystats/dashboard.php
@@ -1,1 +1,33 @@
-Hello World!
+<head>
+    <meta charset="UTF-8">
+    <title>Phystats - Dashboard</title>
+    <link rel="stylesheet" href="css/nav.css" />
+    <link rel="stylesheet" href="css/dashboard.css" />
+    <script type="text/javascript" src="js/script.js"></script>
+    <style>
+        img.action {
+            width: 17px;
+            vertical-align: central;
+        }
+
+        td.none {
+            height: 40px;
+            font-size: 24pt;
+        }
+    </style>
+</head>
+
+<body>
+    <nav>
+        <div>
+            <img class="logo" src="assets/wlogo.png">
+            <h1 class="title">Phystats</h1>
+        </div>
+        <div>
+            <a href="dashboard.php" class="here nav-options">DASHBOARD</a>
+            <a href="#" class="nav-options">STUDENT PERFORMANCE</a>
+            <a href="#" class="nav-options">MANAGE</a>
+            <a href="#" class="nav-options"><img class="profile" src="assets/wprof.png"></a>
+        </div>
+    </nav>
+</body>
